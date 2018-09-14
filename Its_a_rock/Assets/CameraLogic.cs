350c837e6c18a5e31b89f1c9d4606d041d463be8
pijuskri <pijuskri@gmail.com>
2018-09-14 22:22:36 +0300

    simple stuff

---
+++ Its_a_rock/Assets/CameraLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLogic : MonoBehaviour {
+
+    float speed = 0.2f;
+    public GameObject planet;
+    float initialDistance;
+    float movementSpeed = 0.2f;
+    // Use this for initialization
+    void Start()
+    {
+        initialDistance = Vector3.Distance(transform.position, planet.transform.position);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        //transform.RotateAround(planet.transform.position, Vector3.up, x * speed);
+        //transform.RotateAround(planet.transform.position, Vector3.right, y * speed);
+        float convertAnglex = 0;
+        if (transform.rotation.eulerAngles.x <= 180) convertAnglex = transform.rotation.eulerAngles.x;
+        if (transform.rotation.eulerAngles.x > 180) convertAnglex = -(360 - transform.rotation.eulerAngles.x);
+        //print(convertAnglex);
+        if (y > 0 && convertAnglex > 70) y = 0;
+        if (y < 0 && convertAnglex < -70) y = 0;
+
+        transform.Translate(new Vector3(x * speed, y * speed, 0));
+        //transform.rotation = Quaternion.Euler(0, 0, 0);
+        transform.LookAt(planet.transform.position);
+        if (Vector3.Distance(transform.position, planet.transform.position) > initialDistance) transform.Translate(Vector3.forward * 0.04f);
+    }
+}
---
+++ Its_a_rock/Assets/Place.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Place : MonoBehaviour {
+
+    public GameObject building;
+    public Transform planet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        { 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                Vector3 position = Input.mousePosition;
+
+                position = hitInfo.point;
+
+                GameObject temp = Instantiate(building, position, new Quaternion());
+                temp.transform.rotation = Quaternion.LookRotation(2*transform.position - planet.position);
+                temp.transform.parent = planet;
+
+
+            }
+        }
+    }
+}
